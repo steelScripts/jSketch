17422f30ac9b2f6225a255cb823a3c17843e0d5d
steelScripts <asteel22@outlook.com>
2017-05-27 17:37:34 -0400

    Tiles now darken the more scrolled over + bug fixes

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
-	//Grid Generator
+	
+	//Generate Grid with initial white background-color
 	for(i = 0; i < 16; i++){
 		for(j = 0; j < 16; j++) {
-			
-			$("#container").append("<div class='grid'></div>");
-			if(j == 15 && i != 15){$("#container").append("<br>");}
+			$("#container").append("<div class='grid' data-color='255,255,255' data-changed='false'></div>");
+			if(j == 15){$("#container").append("<br>");}
 		}
 	}
 	//CSS Rules
@@ -21,27 +21,67 @@ $(document).ready(function(){
 		"width":"100%"
 	});
 	$(".grid").css({
-				"float":"left",
-				"border":"1px solid black",
-				//"margin":".5em",
-				"width":"1em",
-				"height":"1em"
-			});
-	//Functions
+		"float":"left",
+		"border":"1px solid black",
+		"margin-bottom":"-1em",
+		"width":"1em",
+		"height":"1em"
+	});		
+	//Functions	
 	$(".grid").hover(function(){
-		var r = Math.floor(Math.random() * 255 + 1);
-		var g = Math.floor(Math.random() * 255 + 1);
-		var b = Math.floor(Math.random() * 255 + 1);
-		$(this).css({
-		  "background-color": "rgb("+r+","+g+","+b+")",
-		  "border-color": "rgb("+r+","+g+","+b+")"
-		});
+		var changed = $(this).attr("data-changed"),
+			dataColor = $(this).attr("data-color");
+		if(changed != "true"){
+			var r = Math.floor(Math.random() * 255),
+				g = Math.floor(Math.random() * 255),
+				b = Math.floor(Math.random() * 255),
+				newColor =  r+","+g+","+b
+			$(this).attr("data-color", newColor);
+			$(this).css({			
+			  "background-color": "rgb("+newColor+")",
+			  "border-color": "rgb("+newColor+")"
+			});
+			$(this).attr("data-changed","true");
+		} 
+		else {
+			var commas = 2, r = '', g = '', b = '';
+			
+			for(i = 0; i < dataColor.length; i++){
+				if(dataColor.charAt(i) == ',')
+					{commas--}
+				else if(dataColor.charAt(i) != ',' && commas == 2) 
+					{r += dataColor.charAt(i);}					
+				else if(dataColor.charAt(i) != ',' && commas == 1)
+					{g += dataColor.charAt(i);}					
+				else if(dataColor.charAt(i) != ',' && commas == 0)
+					{b += dataColor.charAt(i);}					
+			}
+			//Converts RGB back to integers
+			var rTemp = r.valueOf();
+			var gTemp = g.valueOf();
+			var bTemp = b.valueOf();
+			
+			//Darkens tile by 10%
+			r = rTemp - Math.floor(rTemp / 10);
+			g = gTemp - Math.floor(gTemp / 10);
+			b = bTemp - Math.floor(bTemp / 10);
+			
+			//Logs new color to array and sets it as the background-color
+			var newColor =  r+","+g+","+b;
+			$(this).attr("data-color", newColor);
+			$(this).css({			
+			  "background-color": "rgb("+newColor+")",
+			  "border-color": "rgb("+newColor+")"
+			});
+		}
+		
 	});
 	$("button").click(function(){
-		$(".grid").css("background-color", "white");
+		$(".grid").attr("data-changed","false");		
+		$(".grid").attr("data-color","255,255,255");
+		$(".grid").css({
+			"background-color": "rgb(255,255,255)",
+			"border-color": "rgb(0,0,0)"
+		});							
 	});
-	
-	
-	
-	
 });
